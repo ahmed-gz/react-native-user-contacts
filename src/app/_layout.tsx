96c20b78a4d7ce4ce246d3b7287b762a7044d39c
Ahmed Gazimi <ahmed.gazimi@gmail.com>
2023-11-21 18:14:27 +0100

    [chore] - Added expo-router and jotai

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,20 @@
+import { SplashScreen } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
